f95b2753d016f72ae6c53f7cbd292e0c33ff3470
Hui Peng <phui@google.com>
2023-12-15 22:55:33 +0000

    Fix: Reland: Fix an OOB write bug in attp_build_value_cmd
    Change-Id: I76bea576b81514916d4800456452c1bbe957fbef

--- stack/gatt/att_protocol.cc
+++ stack/gatt/att_protocol.cc
@@ -285,7 +285,7 @@ BT_HDR* attp_build_value_cmd(uint16_t payload_size, uint8_t op_code,
 #define CHECK_SIZE()                       \
   do {                                     \
     if (size_now > payload_size) {         \
-      LOG_ERROR("payload size too small"); \
+      LOG(ERROR) << "payload size too small"; \
       osi_free(p_buf);                     \
       return nullptr;                      \
     }                                      \
@@ -341,7 +341,7 @@ BT_HDR* attp_build_value_cmd(uint16_t payload_size, uint8_t op_code,
   // backfill pair len field
   if (op_code == GATT_RSP_READ_BY_TYPE) {
     if (pair_len > UINT8_MAX) {
-      LOG_ERROR("pair_len greater than %d", UINT8_MAX);
+      LOG(ERROR) << StringPrintf("pair_len greater than %d", UINT8_MAX);
       osi_free(p_buf);
       return nullptr;
     }
